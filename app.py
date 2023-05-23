e5e91cfd5e064395f7cd4b2bb989e0a7654c6def
Chakradhar <110469081+Drax-dr@users.noreply.github.com>
2023-05-23 19:39:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os, base64
 import flask
+from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask import request, redirect, jsonify
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_login import LoginManager, login_required, UserMixin, login_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime
 
 db = SQLAlchemy()
-login_manager = LoginManager()
+
 app = flask.Flask(__name__,static_folder='static')
 app.config["SECRET_KEY"] = 'abcdefghijklmnopqrstuvwxyz'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
@@ -18,31 +18,26 @@ app.config["SQLALCHEMY_BINDS"] = {
     "Admin":"sqlite:///admin.db"
 }
 app.config['UPLOAD_FOLDER'] = './static/images'
-login_manager.init_app(app=app)
 db = SQLAlchemy(app=app)
 ma = Marshmallow(app=app)
 admin_user = Admin(app=app)
+auth = HTTPBasicAuth()
 
-class Admin(db.Model, UserMixin):
+class Admin(db.Model):
     __bind_key__ = 'Admin'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.Text(), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False, unique=True)
     date_joined = db.Column(db.Date, default=datetime.utcnow)
     image = db.Column(db.String(120))
     about = db.Column(db.Text, nullable=False, unique=True)
-
-    @property
-    def password(self):
-        return AttributeError("Password is n0t a readable format!")
     
-    @password.setter
-    def password(self,password):
-        self.password = generate_password_hash(password)
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
         print(password)
-        return check_password_hash(str(self.password), str(password))
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return "<User %s>" % self.username
@@ -68,10 +63,6 @@ class PostSchema(ma.SQLAlchemyAutoSchema):
         load_instance = True
     fields = ("id", "title", "content", "date_posted", "image")
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Admin.query.get(user_id)
-
 @app.route("/")
 def home():
     all_posts = Posts.query.all()
@@ -93,12 +84,12 @@ def get(pg_id):
     return jsonify(data)
 
 @app.route("/cur", methods = ['GET','POST'])
-@login_required
+@auth.login_required
 def cur():
-    return "YOU ARE "+current_user.username
+    return "YOU ARE "+auth.username()
 
 @app.route("/add", methods = ['GET','POST'])
-@login_required
+@auth.login_required
 def add():
     if request.method == "GET":
         return "<h1>404 Not Found</h1>"
@@ -118,8 +109,8 @@ def add():
     db.session.commit()
     return redirect("/")
 
-@app.route("/delete/<int:pg_id>", methods = ['POST'])
-@login_required
+@app.route("/delete/<int:pg_id>", methods = ['GET','POST'])
+@auth.login_required
 def delete(pg_id):
     pg_id = pg_id
     blog = Posts.query.get(pg_id)
@@ -128,7 +119,7 @@ def delete(pg_id):
     return redirect("/")
 
 @app.route("/update/<int:pg_id>", methods = ['POST'])
-@login_required
+@auth.login_required
 def update(pg_id):
     pg_id = pg_id
     blog = Posts.query.get(pg_id)
@@ -136,26 +127,30 @@ def update(pg_id):
     blog.content = request.form.get("content")
     blog.image = request.form.get("image")
     db.session.commit()
-
     return redirect("/")
 
-@app.route("/admin_login", methods=["POST"])
+@auth.verify_password
+def verify_password(user, passwd):
+    admin = db.session.query(Admin).filter_by(username=user).first()
+    print(admin)
+    if admin:
+        if check_password_hash(admin.password_hash, passwd):
+            res = True
+        else:
+            res = False
+    else:
+        res = False
+    return res
+
+@app.route("/check_admin", methods=["POST"])
 def get_admin():
     form = request.form
     user = form["username"]
     passwd = form["password"]
-    print(user,passwd)
-    admin = db.session.query(Admin).filter_by(username=user).first()
-    print(admin)
-    if admin:
-        if check_password_hash(str(admin.password), str(passwd)):
-            login_user(admin)
-            res = jsonify({'message':'You are Logged in'})
-        else:
-            res = jsonify({'Error':'Invalid password'})
-    else:
-        res = jsonify({'Error':'You are not admin'})
-    return res
+    vr = verify_password(user, passwd)
+    if vr:
+        return jsonify({'message':"You are admin!"})
+    return jsonify({'Error':"You are not admin"})
 
 if __name__ == "__main__":
     app.run()
